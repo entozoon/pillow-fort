83e3ecefcd70f6cfc7abdbc592c8ddbb1009061d
Mickey <michael.cook@techdept.co.uk>
2021-12-20 23:04:59 +0000

    Okay so I've no idea how dt and game loop slots in but.. yeah. Maybe I don't even need one

--- lib/game.ts
+++ lib/game.ts
@@ -15,20 +15,26 @@ export default class Game {
     this.canvas = canvas;
     this.engine = new Engine(this.canvas, true);
     this.scene = new Scene(this.engine);
-    const cameraPos = new Vector3(0, 5, -10);
-    this.camera = new FreeCamera("maincam", cameraPos, this.scene);
+    this.camera = new FreeCamera("camera", new Vector3(0, 5, -10), this.scene);
     this.camera.setTarget(Vector3.Zero());
-    this.camera.attachControl(this.canvas, false);
-    const lightPos = new Vector3(0, 1, 0);
-    new HemisphericLight("hemlight", lightPos, this.scene);
-    const sphereOpts = { segments: 16, diameter: 1 };
-    let sphere = MeshBuilder.CreateSphere("mainsphere", sphereOpts, this.scene);
-    sphere.position.y = 1;
-    const groundOpts = { width: 6, height: 6, subdivisions: 2 };
-    MeshBuilder.CreateGround("mainground", groundOpts, this.scene);
-    this.animate();
-  }
-  animate() {
-    this.engine.runRenderLoop(() => this.scene.render());
+    new HemisphericLight("hemlight", new Vector3(0, 1, 0), this.scene);
+    let sphere = MeshBuilder.CreateSphere(
+      "testSphere",
+      { segments: 16, diameter: 1 },
+      this.scene
+    );
+    MeshBuilder.CreateGround(
+      "mainground",
+      { width: 6, height: 6, subdivisions: 2 },
+      this.scene
+    );
+    let i = 0;
+    // setInterval(() => {
+    // }, 100);
+    this.engine.runRenderLoop(() => {
+      sphere.position.y = Math.sin(i) * 2;
+      i++;
+      this.scene.render();
+    });
   }
 }
