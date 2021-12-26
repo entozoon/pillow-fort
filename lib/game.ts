999f27f66b28f40770fb405007d1d80e381511db
Mickey <michael.cook@techdept.co.uk>
2021-12-26 22:29:37 +0000

    Types, docs and versions are all over the place.. classic gamedev lib, sigh

--- lib/game.ts
+++ lib/game.ts
@@ -1,11 +1,15 @@
 import {
+  ArcRotateCamera,
   Engine,
   FreeCamera,
   HemisphericLight,
+  Mesh,
   MeshBuilder,
   Scene,
+  StandardMaterial,
+  Texture,
   Vector3,
-} from "babylonjs";
+} from "@babylonjs/core";
 export default class Game {
   canvas!: HTMLCanvasElement;
   engine!: Engine;
@@ -18,24 +22,32 @@ export default class Game {
     this.camera = new FreeCamera("camera", new Vector3(0, 5, -10), this.scene);
     this.camera.setTarget(Vector3.Zero());
     new HemisphericLight("hemlight", new Vector3(1, 1, 0), this.scene);
+    MeshBuilder.CreateTiledGround(
+      "floor",
+      { xmin: -1, zmin: -1, xmax: 1, zmax: 1, subdivisions: { w: 1, h: 1 } },
+      this.scene
+    );
     let sphere = MeshBuilder.CreateSphere(
       "testSphere",
       { segments: 16, diameter: 1 },
       this.scene
     );
-    let box = MeshBuilder.CreateBox(
-      "boxy",
-      {
-        size: 4,
-      },
-      this.scene
-    );
+    let box = MeshBuilder.CreateBox("boxy", {
+      size: 4,
+    });
     box.position.y = 2;
-    MeshBuilder.CreateGround(
-      "mainground",
-      { width: 6, height: 6, subdivisions: 2 },
+    // let tiledBox = MeshBuilder.CreateBox("boxy", {
+    //   sideOrientation: BABYLON.Mesh.DOUBLESIDE,
+    //   width: 6,
+    //   height: 4,
+    //   depth: 3,
+    // });
+    let material = new StandardMaterial("bricks", this.scene);
+    material.diffuseTexture = new Texture(
+      "https://assets.babylonjs.com/environments/bricktile.jpg",
       this.scene
     );
+    box.material = material;
     let i = 0;
     // setInterval(() => {
     // }, 100);
