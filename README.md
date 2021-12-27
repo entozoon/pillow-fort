27ffcd9351d696eeafb0b08b0569527ec71dd997
Mickey <michael.cook@techdept.co.uk>
2021-12-27 13:06:58 +0000

    Ammo physics but box appears to shatter on impact..

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 ## Dev Notes
 
 Using sockets? try https://github.com/geckosio/geckos.io#usage instead of socket.io
+
+## Ideas
+
+- Headlamp light
--- lib/game.ts
+++ lib/game.ts
@@ -1,3 +1,5 @@
+import Ammo from "ammojs-typed";
+import { AmmoJSPlugin } from "@babylonjs/core/Physics/Plugins/ammoJSPlugin";
 import {
   ArcRotateCamera,
   Engine,
@@ -10,61 +12,91 @@ import {
   StandardMaterial,
   Texture,
   Vector3,
+  PhysicsImpostor,
 } from "@babylonjs/core";
 export default class Game {
   canvas!: HTMLCanvasElement;
   engine!: Engine;
   scene!: Scene;
   camera!: FreeCamera;
+  ground!: Mesh;
+  light!: HemisphericLight;
   constructor({ canvas }: { canvas: HTMLCanvasElement }) {
-    this.canvas = canvas;
-    this.engine = new Engine(this.canvas, true);
-    this.scene = new Scene(this.engine);
-    // this.camera = new FreeCamera("camera", new Vector3(0, 5, -10), this.scene);
-    // this.camera.setTarget(Vector3.Zero());
-    new HemisphericLight("hemlight", new Vector3(1, 1, 0), this.scene);
-    MeshBuilder.CreateTiledGround(
-      "floor",
-      { xmin: -1, zmin: -1, xmax: 1, zmax: 1, subdivisions: { w: 1, h: 1 } },
+    Ammo()
+      .then((api) => {
+        this.canvas = canvas;
+        this.engine = new Engine(this.canvas, true);
+        this.scene = new Scene(this.engine);
+        this.scene.enablePhysics(
+          new Vector3(0, -9.81, 0),
+          // Ammo as IPhysicsEnginePlugin
+          new AmmoJSPlugin()
+        );
+      })
+      .then(() => {
+        this.light = new HemisphericLight(
+          "hemlight",
+          new Vector3(1, 1, 0),
+          this.scene
+        );
+        this.light.intensity = 0.8;
+        this.initGround();
+        let box = MeshBuilder.CreateBox("boxy", {
+          size: 1,
+        });
+        box.position.y = 4;
+        let material = new StandardMaterial("bricks", this.scene);
+        material.diffuseTexture = new Texture(
+          "https://assets.babylonjs.com/environments/bricktile.jpg",
+          this.scene
+        );
+        box.material = material;
+        const softBoxOptions = {
+          mass: 5,
+          pressure: 0,
+        };
+        box.physicsImpostor = new PhysicsImpostor(
+          box,
+          PhysicsImpostor.SoftbodyImpostor,
+          softBoxOptions,
+          this.scene
+        );
+        this.initCamera(box);
+        let i = 0;
+        this.engine.runRenderLoop(() => {
+          box.rotation = new Vector3(0, Math.sin(i / 100) * 2, 0);
+          box.position.x = Math.sin(i / 100) * 2;
+          i++;
+          this.scene.render();
+        });
+      });
+  }
+  private initGround() {
+    this.ground = MeshBuilder.CreateBox(
+      "ground",
+      { width: 10, depth: 10, height: 1 },
       this.scene
     );
-    let sphere = MeshBuilder.CreateSphere(
-      "testSphere",
-      { segments: 16, diameter: 1 },
+    var bleh = new StandardMaterial("bleh", this.scene);
+    bleh.diffuseTexture = new Texture(
+      "https://www.babylonjs-playground.com/textures/ground.jpg",
       this.scene
     );
-    let box = MeshBuilder.CreateBox("boxy", {
-      size: 4,
-    });
-    box.position.y = 4;
-    // let tiledBox = MeshBuilder.CreateBox("boxy", {
-    //   sideOrientation: BABYLON.Mesh.DOUBLESIDE,
-    //   width: 6,
-    //   height: 4,
-    //   depth: 3,
-    // });
-    let material = new StandardMaterial("bricks", this.scene);
-    material.diffuseTexture = new Texture(
-      "https://assets.babylonjs.com/environments/bricktile.jpg",
+    this.ground.material = bleh;
+    this.ground.physicsImpostor = new PhysicsImpostor(
+      this.ground,
+      PhysicsImpostor.BoxImpostor,
+      { mass: 0, friction: 0.5, restitution: 0 },
       this.scene
     );
-    box.material = material;
-    let i = 0;
-    // setInterval(() => {
-    // }, 100);
-
+  }
+  private initCamera(box: Mesh) {
     const camera = new FollowCamera(
       "FollowCam",
-      new Vector3(-6, 0, 0),
+      new Vector3(-100, 0, 0),
       this.scene
     );
     camera.lockedTarget = box;
-    this.engine.runRenderLoop(() => {
-      box.rotation = new Vector3(0, Math.sin(i / 100) * 2, 0);
-      box.position.x = Math.sin(i / 100) * 2;
-      // sphere.position.y = Math.sin(i) * 2;
-      i++;
-      this.scene.render();
-    });
+    camera.radius = 5;
   }
 }
